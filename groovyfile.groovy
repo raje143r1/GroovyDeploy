bbe07af813ac1e22bd5ec4e0258e7bc2e74381ad
raje143r1 <74051249+raje143r1@users.noreply.github.com>
2022-10-13 21:40:33 +0530

    Update groovyfile.groovy

--- groovyfile.groovy
+++ groovyfile.groovy
@@ -4,6 +4,7 @@ pipeline{
               stage("build"){
                steps{
                 echo 'helloworld'
+                       readProp = readProperties file:
                 }
                }
                    
