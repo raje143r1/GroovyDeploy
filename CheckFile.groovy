01d691edb1dd03ff25b2503f70c6f765545de949
raje143r1 <74051249+raje143r1@users.noreply.github.com>
2022-10-16 08:00:19 +0530

    Update CheckFile.groovy

--- CheckFile.groovy
+++ CheckFile.groovy
@@ -13,19 +13,12 @@ pipeline{
 						   script{
 								
 						       if(ArtifacatstoDeploy == 'EarandXML'){
-								withAnt(installation: 'Ant 1.8.4', jdk: 'Zulu OpenJDK 8.0.302') {
-									 wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${env.pwd}", var: 'PSWD'],[password: "${env.TIBCOPwd}", var: 'TPSWD']]]) {
-									sh "ant -f \"${workspace}\"@script/abc27b879062018c45d2aa53e75587cc2d6cc6eed65da714bf3caff22484a027/TIBCO/build_test.xml PreDeploymentStep UploadEAR UploadXML"	
-									
+								   echo '${env.filename}'
 									}
 								}
 							 }
 							 else if(ArtifacatstoDeploy == 'isOnlyEAR'){
-							 withAnt(installation: 'Ant 1.8.4', jdk: 'Zulu OpenJDK 8.0.302') {
-									 wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${env.pwd}", var: 'PSWD'],[password: "${env.TIBCOPwd}", var: 'TPSWD']]]) {
-									sh "ant -f \"${workspace}\"@script/abc27b879062018c45d2aa53e75587cc2d6cc6eed65da714bf3caff22484a027/TIBCO/DeployOnlyEar.xml PreDeploymentStep UploadEAR"	
-									}
-								}
+							  echo '${env.filename}'
 						   }
 					   }
 					}
